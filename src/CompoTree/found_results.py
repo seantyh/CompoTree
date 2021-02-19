562b6bcd291dbe3467a25482901845b028555e2d
seantyh <seantyh@gmail.com>
2021-02-19 22:51:58 +0800

    add additional method to CTFounds

--- src/CompoTree/found_results.py
+++ src/CompoTree/found_results.py
@@ -1,11 +1,39 @@
 
+from CompoTree.ts_variants import TSVariants
+
+
 class CTFounds:
     def __init__(self, hits_list):
         self.results = hits_list
+        self.tsvars = None
 
     def __repr__(self):
         return f"<CTFounds: {len(self.results)} entries>"
 
     def filter(self, idc="", pos="", flag=""):
-        return [x for x in self.results
-                if x[1][-1].match(idc, pos, flag)]
+        res = [x for x in self.results
+               if x[1][-1].match(idc, pos, flag)]
+        return CTFounds(res)
+    
+    def filter_with_lexicon(self, lexicon):
+        res = [x for x in self.results
+               if x[0] in lexicon]
+        return CTFounds(res)
+
+    def simplified_only(self):
+        if not self.tsvars:
+            self.tsvars = TSVariants.load()
+        
+        res = [x for x in self.results
+               if self.tsvars.is_simplified(x[0])]
+        return CTFounds(res)
+    
+    def traditional_only(self):
+        if not self.tsvars:
+            self.tsvars = TSVariants.load()
+        res = [x for x in self.results
+               if not self.tsvars.is_simplified(x[0])]
+        return CTFounds(res)
+    
+    def tolist(self):
+        return self.results
